73534eb9f6ed3925d5448c9d471573a9acf2e787
aliforever <alipadida@live.com>
2021-04-03 04:16:12 +0430

    fix bug

--- telegrambot.go
+++ telegrambot.go
@@ -296,7 +296,8 @@ func (tb *TelegramBot) StopReceivingUpdates() {
 
 func (tb *TelegramBot) ListenWebhook(address string) (err error) {
 	tb.updatesChannel = make(chan *structs.Update)
-	http.HandleFunc(fmt.Sprintf("%s", tb.apiToken), func(writer http.ResponseWriter, request *http.Request) {
+	http.HandleFunc(fmt.Sprintf("/%s", tb.apiToken), func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("new update")
 		defer request.Body.Close()
 		var u *structs.Update
 		j := json.NewDecoder(request.Body)
